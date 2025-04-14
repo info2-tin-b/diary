749950745cd74cfce4f02f9daf8467660540cb77
Yves Chevallier <yves.chevallier@heig-vd.ch>
2025-04-14 14:46:27 +0200

    Final push-back

--- 08/linked-list.c
+++ 08/linked-list.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h> 
 
 struct node {
     int data;
@@ -7,27 +8,41 @@ struct node {
 
 typedef struct node Node;
 
-void display(Node *el) {
+typedef struct list {
+    Node *head;
+    Node *tail;
+} List;
+
+void display(List *list) {
+    Node *el = list->head;
     while(el) {
-        printf("%c\n", el->data);
+        printf("%d\n", el->data);
         el = el->next;
     }
 }
 
-Node * push_back(Node* head, int value) {
+Node * push_back(List *list, int data) {
+    Node *new_node = (Node*)calloc(1, sizeof(Node));
+    if (new_node == NULL) exit(EXIT_FAILURE);
+    new_node->data = data;
 
+    // If list is empty?
+    if (list->tail == NULL || list->head == NULL) {
+        list->head = list->tail = new_node;
+        return new_node;
+    }
+    list->tail->next = new_node;
+    list->tail = list->tail->next;
+    return new_node;
 }
 
 int main() {
-    // T0
-    Node c = {.data = 'c', .next = NULL};
-    Node b = {.data = 'b', .next = &c };
-    Node a = {.data = 'a', .next = &b };
-    Node *head = &a;
-
-    // T1 Insérer l'élément `d` au début de la liste
-    Node d = {.data = 'd', .next = &a };
-    head = &d;
-
-    display(head);
+    List list = {0};
+    push_back(&list, 4);
+    push_back(&list, 8);
+    push_back(&list, 15);
+    push_back(&list, 16);
+    push_back(&list, 23);
+    push_back(&list, 42);
+    display(&list);
 }
